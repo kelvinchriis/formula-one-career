1d3c95244e1ae55806b52f81072ddb7a0bba04ed
Kelvin Christian <kelvin.santos@sptech.school>
2023-12-04 05:40:06 -0300

    adicionando inserts de perfil ao script

--- site/src/database/script_modelagem.sql
+++ site/src/database/script_modelagem.sql
@@ -1,13 +1,6 @@
 create database formula_one_carrer;
 use formula_one_carrer;
 
-create table usuario (
-	idUsuario int primary key auto_increment,
-    nome varchar(50),
-    email varchar(45),
-    senha varchar(45)
-);
-
 create table equipe (
 	idEquipe int primary key,
     nome varchar(45),
@@ -24,44 +17,56 @@ create table carro (
     constraint fkEquipeCarro foreign key(fkEquipeCarro) references equipe(idEquipe)
 );
 
-create table pista (
-	idPista int primary key,
-    nome varchar(45),
-    pais varchar(45),
-    comprimento float,
-    qtaVolta int
-);
-
 create table piloto (
 	idPiloto int primary key,
     nome varchar(50),
     nacionalidade varchar(45),
     qtdTitulo int,
-    dataNasc date,
-    idadeInicio int,
-    fkEquipePiloto int,
-    constraint fkEquipePiloto foreign key(fkEquipePiloto) references equipe(idEquipe)
+    numeroCarro int
+);
+
+create table pista (
+	idPista int primary key,
+    nome varchar(45),
+    anoInicio int,
+    qtaVolta int,
+    comprimentoCiruito float,
+    comprimentoCorrida float,
+    voltaRapida varchar(100)
 );
 
 create table perfil (
 	idPerfil int primary key auto_increment,
+    username varchar(45),
     dataNasc date,
     nacionalidade varchar(45),
-    idadeInicio int,
+    numeroCarro int,
     biografia varchar(500),
+    fkEquipe int,
+    fkCarro int,
     fkPistaPreferida int,
     fkPilotoInspiracao int,
+    imagem varchar(255);
     constraint fkPistaPrefe foreign key(fkPistaPreferida) references pista(idPista),
-    constraint fkInspiracao foreign key(fkPilotoInspiracao) references piloto(idPiloto)
+    constraint fkInspiracao foreign key(fkPilotoInspiracao) references piloto(idPiloto),
+    constraint fkEquipe foreign key(fkEquipe) references equipe(idEquipe),
+    constraint fkCarro foreign key(fkCarro) references  carro(idCarro)
 );
 
-create table carreira (
-	idCarreira int auto_increment,
-    fkUsuario int,
-    primary key(idCarreira, fkUsuario),
-    fkPerfil int,
-    fkEquipe int,
-    constraint fkUser foreign key(fkUsuario) references usuario(idUsuario),
-    constraint fkPerfil foreign key(fkPerfil) references perfil(idPerfil),
-    constraint fkEquipe foreign key(fkEquipe) references equipe(idEquipe)
+create table usuario (
+	idUsuario int primary key auto_increment,
+    nome varchar(50),
+    email varchar(45),
+    senha varchar(45),
+    fk_perfil int,
+    constraint fkPerfil foreign key(fk_perfil) references perfil(idPerfil)
+);
+
+create table comentarios (
+    idComentario int primary key auto_increment,
+    comentario varchar(1000),
+    dataComent date,
+    imagem varchar(255),
+    fkPerfilComent int,
+    constraint  fkPerfilComent foreign key(fkPerfilComent) references perfil(idPerfil)
 );
