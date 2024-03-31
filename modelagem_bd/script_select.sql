c5bc7f3e29ea87d80542689b78451766ef2d0aeb
Kelvin Christian <kelvinchrisrodrigues@gmail.com>
2024-03-31 01:00:17 -0300

    Delete modelagem_bd/script_select.sql

--- modelagem_bd/script_select.sql
+++
@@ -1,5 +0,0 @@
--- mostra o o nome das equipes e seus repectivos carros de acordo com os anos
-SELECT e.nome, c.modelo, c.ano FROM carro as c JOIN equipe as e on fkEquipeCarro = idEquipe;
-
--- mostrando os mesmos dados de anteriormente, porém apenas de uma equipe(altere o nome)
-SELECT e.nome, c.modelo, c.ano FROM carro as c JOIN equipe as e on fkEquipeCarro = idEquipe where e.nome = "Ferrari";
